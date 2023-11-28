1451c1828b0db7d8f31ef833114829c85161a44a
Sebastian <sebastian99private@icloud.com>
2023-11-28 13:42:50 +0100

    Comments made for Physio class (Model Layer)

--- GTFys/Models/Physio.cs
+++ GTFys/Models/Physio.cs
@@ -6,54 +6,31 @@ using System.Threading.Tasks;
 
 namespace GTFys.Models
 {
+    // The Physio class represents information about a physiotherapist
     public class Physio
     {
-        public int PhysioID { get; set; }
+        // Properties representing various attributes of a physiotherapist
+        public int PhysioID { get; set; }                // Unique identifier for the physiotherapist
+        public string PhysioCPR { get; set; }            // CPR number of the physiotherapist
+        public string PhysioUsername { get; set; }       // Username chosen by the physiotherapist
+        public string PhysioPassword { get; set; }       // Password for physiotherapist authentication
+        public string PhysioProfilePicture { get; set; } // Profile picture uploaded by physiotherapist or generated 
+        public int PhysioPhoneNumber { get; set; }       // Phone number of the physiotherapist
+        public string PhysioEmail { get; set; }          // Email address of the physiotherapist
+        public string PhysioAddress { get; set; }        // Residential address of the physiotherapist
+        public string PhysioCity { get; set; }           // City of residence for the physiotherapist
+        public int PhysioPostalCode { get; set; }        // Postal code of the physiotherapist's location
 
-        public string PhysioCPR {  get; set; }
-
-        public string PhysioUsername { get; set; }
-
-        public string PhysioPassword { get; set; }
-
-        public string PhysioProfilePicture { get; set; }
-
-        public int PhysioPhoneNumber { get; set; }
-
-        public string PhysioEmail { get; set; }
-
-        public string PhysioAddress { get; set; }
-
-        public string PhysioCity { get; set; }
-
-        public int PhysioPostalCode { get; set; }
-
-        // Constructor to initiliaze physio properties
-        public Physio(int physioID, string physioCPR, string physioUsername, string physioPassword,
-            int physioPhoneNumber, string physioEmail, string physioAddress, string physioCity, int physioPostalCode)
-        {
-            // Set the values of physio properties based on constructor parameters
-            PhysioID = physioID;
-            PhysioCPR = physioCPR;
-            PhysioUsername = physioUsername;
-            PhysioPassword = physioPassword;
-            PhysioPhoneNumber = physioPhoneNumber;
-            PhysioEmail = physioEmail;
-            PhysioAddress = physioAddress;
-            PhysioCity = physioCity;
-            PhysioPostalCode = physioPostalCode;
-        }
-
-        // Overloaded constructor to initialize physio properties with image 
+        // Constructor to initialize a Physio object with essential attributes
         public Physio(int physioID, string physioCPR, string physioUsername, string physioPassword, string imagePath,
             int physioPhoneNumber, string physioEmail, string physioAddress, string physioCity, int physioPostalCode)
         {
-            // Set the values of physio properties based on constructor parameters
+            // Set the properties of the Physio object based on the provided parameters
             PhysioID = physioID;
             PhysioCPR = physioCPR;
             PhysioUsername = physioUsername;
             PhysioPassword = physioPassword;
-            PhysioProfilePicture = imagePath;
+            PhysioProfilePicture = imagePath; //  imagePath: Path to the profile picture of the physiotherapist
             PhysioPhoneNumber = physioPhoneNumber;
             PhysioEmail = physioEmail;
             PhysioAddress = physioAddress;
@@ -61,4 +38,5 @@ namespace GTFys.Models
             PhysioPostalCode = physioPostalCode;
         }
     }
+
 }
