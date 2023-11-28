02a1f349613afbf97eb7162b4aa4e72e997feb91
Sebastian <sebastian99private@icloud.com>
2023-11-28 13:46:18 +0100

    Comments made for TreatmentType class (Model Layer)

--- GTFys/Models/TreatmentType.cs
+++ GTFys/Models/TreatmentType.cs
@@ -6,21 +6,22 @@ using System.Threading.Tasks;
 
 namespace GTFys.Models
 {
+    // The TreatmentType class represents a type of treatment with specific attributes
     public class TreatmentType
     {
-        private string Type { get; }
+        // Private properties representing attributes of a treatment type
+        private string Type { get; set; }       // The type or name of the treatment
+        private TimeSpan Time { get; set; }     //The duration of the treatment (represented as a TimeSpan)
+        private decimal Cost { get; set; }      // The cost of the treatment
 
-        private TimeSpan Time { get; }
-
-        private decimal Cost { get; }
-
-        // Constructor to initiliaze TreatmentType properties
+        // Constructor to initialize a TreatmentType object
         public TreatmentType(string type, TimeSpan time, decimal cost)
         {
+            // Set the private properties of the TreatmentType object based on the provided parameters
             Type = type;
             Time = time;
             Cost = cost;
         }
-
     }
+
 }
