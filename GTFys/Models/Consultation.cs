fce3c39233de526ff751ec7b6891d08ccf7a3aea
Sebastian <sebastian99private@icloud.com>
2023-11-28 13:25:02 +0100

    Comments made for Consultation class (Model Layer)

--- GTFys/Models/Consultation.cs
+++ GTFys/Models/Consultation.cs
@@ -6,23 +6,23 @@ using System.Threading.Tasks;
 
 namespace GTFys.Models
 {
+    // The Consultation class represents a consultation session
     public class Consultation
     {
-        public Patient Patient { get; set; }
-        
-        public Physio Physio { get; set; }
+        // Properties for the Patient, Physio, TreatmentType, and DateTime of the consultation
+        public Patient Patient { get; set; } // The patient involved in the consultation
+        public Physio Physio { get; set; } // The physiotherapist involved in the consultation
+        public TreatmentType TreatmentType { get; set; } // The type of treatment provided during the consultation
+        public DateTime DateAndTime { get; set; } // The date and time of the consultation
 
-        public TreatmentType TreatmentType { get; set; }
-        
-        public DateTime DateTime { get; set; }
-
-        // Constructor to initiliaze Consultation properties
+        // Constructor to initialize Consultation properties
         public Consultation(Patient patient, Physio physio, TreatmentType treatmenType, DateTime dateTime)
         {
+            // Set the properties of the Consultation object based on the provided parameters
             Patient = patient;
             Physio = physio;
             TreatmentType = treatmenType;
-            DateTime = dateTime;
+            DateAndTime = dateTime;
         }
     }
 }
