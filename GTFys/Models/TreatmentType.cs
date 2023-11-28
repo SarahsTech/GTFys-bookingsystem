012cab6860faea06ead25e069d630ed6cc1344b1
Sarah Beckwith <purposewithsarah@gmail.com>
2023-11-28 10:37:29 +0100

    Created TreatmentType.cs and added properties + constructor

---
+++ GTFys/Models/TreatmentType.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GTFys.Models
+{
+    public class TreatmentType
+    {
+        private string Type { get; }
+
+        private TimeSpan Time { get; }
+
+        private decimal Cost { get; }
+
+        // Constructor to initiliaze TreatmentType properties
+        public TreatmentType(string type, TimeSpan time, decimal cost)
+        {
+            Type = type;
+            Time = time;
+            Cost = cost;
+        }
+
+    }
+}
