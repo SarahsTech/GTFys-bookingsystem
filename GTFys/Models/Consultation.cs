6271d8d82253de7aaac9359e18f209ac04988522
Sarah Beckwith <purposewithsarah@gmail.com>
2023-11-28 10:42:12 +0100

    Set up Consultation properties + constructor

--- GTFys/Models/Consultation.cs
+++ GTFys/Models/Consultation.cs
@@ -8,6 +8,21 @@ namespace GTFys.Models
 {
     public class Consultation
     {
+        public Patient Patient { get; set; }
+        
+        public Physio Physio { get; set; }
 
+        public TreatmentType TreatmentType { get; set; }
+        
+        public DateTime DateTime { get; set; }
+
+        // Constructor to initiliaze Consultation properties
+        public Consultation(Patient patient, Physio physio, TreatmentType treatmenType, DateTime dateTime)
+        {
+            Patient = patient;
+            Physio = physio;
+            TreatmentType = treatmenType;
+            DateTime = dateTime;
+        }
     }
 }
