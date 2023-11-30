5e14d4fbdec079a953c24680dcf203fdaea85f91
Tobbekjaer <78278904+Tobbekjaer@users.noreply.github.com>
2023-11-30 13:31:20 +0100

    Deleted Physio and Patient prefix in classes.

--- GTFys/Models/Physio.cs
+++ GTFys/Models/Physio.cs
@@ -10,44 +10,45 @@ namespace GTFys.Models
     public class Physio
     {
         // Properties representing various attributes of a physiotherapist
-        public int PhysioID { get; set; }                // Unique identifier for the physiotherapist
-        public string PhysioCPR { get; set; }            // CPR number of the physiotherapist
-        public string PhysioFirstName { get; set; }      // First name of the physiotherapist
-        public string PhysioLastName { get; set; }       // Last name of the physiotherapist
-        public string PhysioUsername { get; set; }       // Username chosen by the physiotherapist
-        public string PhysioPassword { get; set; }       // Password for physiotherapist authentication
-        public int PhysioPhoneNumber { get; set; }       // Phone number of the physiotherapist
-        public string PhysioEmail { get; set; }          // Email address of the physiotherapist
-        public string PhysioAddress { get; set; }        // Residential address of the physiotherapist
-        public string PhysioCity { get; set; }           // City of residence for the physiotherapist
-        public int PhysioZipCode { get; set; }        // Postal code of the physiotherapist's location
-        public string PhysioProfilePicture { get; set; } // Profile picture uploaded by physiotherapist or generated 
+        public int PhysioID { get; set; }              // Unique identifier for the physiotherapist
+        public string CPR { get; set; }                // CPR number of the physiotherapist
+        public string FirstName { get; set; }          // First name of the physiotherapist
+        public string LastName { get; set; }           // Last name of the physiotherapist
+        public string Username { get; set; }           // Username chosen by the physiotherapist
+        public string Password { get; set; }           // Password for physiotherapist authentication
+        public string PhoneNumber { get; set; }           // Phone number of the physiotherapist
+        public string Email { get; set; }              // Email address of the physiotherapist
+        public string Address { get; set; }            // Residential address of the physiotherapist
+        public string City { get; set; }               // City of residence for the physiotherapist
+        public int ZipCode { get; set; }               // Postal code of the physiotherapist's location
+        public string ProfilePicture { get; set; }     // Profile picture uploaded by physiotherapist or generated 
 
         // Constructor to initialize a Physio object with essential attributes
-        public Physio(string physioCPR, string physioFirstName, string physioLastName, string physioUsername, string physioPassword,
-            int physioPhoneNumber, string physioEmail, string physioAddress, string physioCity, int physioZipCode, string imagePath)
+        public Physio(string cpr, string firstName, string lastName, string username, string password,
+            string phoneNumber, string email, string address, string city, int zipCode, string imagePath)
         {
             // Set the properties of the Physio object based on the provided parameters
-            PhysioCPR = physioCPR;
-            PhysioFirstName = physioFirstName;
-            PhysioLastName = physioLastName;
-            PhysioUsername = physioUsername;
-            PhysioPassword = physioPassword;
-            PhysioPhoneNumber = physioPhoneNumber;
-            PhysioEmail = physioEmail;
-            PhysioAddress = physioAddress;
-            PhysioCity = physioCity;
-            PhysioZipCode = physioZipCode;
-            PhysioProfilePicture = imagePath; //  imagePath: Path to the profile picture of the physiotherapist
+            CPR = cpr;
+            FirstName = firstName;
+            LastName = lastName;
+            Username = username;
+            Password = password;
+            PhoneNumber = phoneNumber;
+            Email = email;
+            Address = address;
+            City = city;
+            ZipCode = zipCode;
+            ProfilePicture = imagePath; //  imagePath: Path to the profile picture of the physiotherapist
         }
 
         // Overloaded constructor to authenticate login
-        public Physio(string physioUsername, string physioPassword)
+        public Physio(string username, string password)
         {
             // Set the properties of the Physio object based on the provided parameters
-            PhysioUsername = physioUsername;
-            PhysioPassword = physioPassword;
+            Username = username;
+            Password = password;
         }
     }
+
 
 }
