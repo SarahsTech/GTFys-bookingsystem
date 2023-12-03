57528c1d9b27e0cd8803024ea7944257220d1eea
Tobbekjaer <78278904+Tobbekjaer@users.noreply.github.com>
2023-12-03 09:48:12 +0100

    Changes in patient

--- GTFys/ViewModels/PatientRepo.cs
+++ GTFys/ViewModels/PatientRepo.cs
@@ -2,6 +2,7 @@
 using System;
 using System.Collections.Generic;
 using System.Data;
+using System.Diagnostics;
 using System.IO;
 using System.Linq;
 using System.Text;
@@ -31,52 +32,49 @@ namespace GTFys.ViewModels
             // The result set of AuthenticateLoginAsync
             bool isAuthenticated = result.isAuthenticated;
 
-        // public async Task<bool> patientAuthenticateLogin(string username, string password)
-        //{
-        //    // Create a new Patient object with the provided username and password
-        //    Patient patient = new Patient(username, password);
-
-        //    // Call the generic AuthenticateLoginAsync method in DatabaseAccess
-        //    // to perform the authentication for the patient
-        //    bool patientAuthResult = await dbAccess.AuthenticateLoginAsync(patient);
-
-        //    // Return the result of the authentication (true if successful, false otherwise)
-        //    return patientAuthResult;
-        //}
-
-        // Method to insert a new patient using the stored procedure gtspInsertPatient
-        // Returns a boolean indicating whether the insertion was successful or not
-        public async Task<bool> PatientCreateUser(Patient patient)
-        {
-            bool insertionSuccessful = false;
-            try {
-                // Stored procedure name
-                var storedProcedure = "gtspInsertPatient";
-
-                // Parameters for the stored procedure, using the properties of the Patient object
-                var parameters = new {
-                    patient.FirstName,
-                    patient.LastName,
-                    patient.Username,
-                    patient.Password,
-                    patient.CPR,
-                    patient.Phone,
-                    patient.Email,
-                    patient.Address,
-                    patient.ZipCode,
-                    patient.City,
-                    patient.ProfilePicture,
-                };
-
-
-            // Execute the stored procedure and retrieve the output parameter
-            var rowsAffected = await dbAccess.ExecuteNonQueryAsync(storedProcedure, parameters, CommandType.StoredProcedure);
-
             // Set the CurrentPatient to the authenticated patient
             PatientService.CurrentPatient = (Patient)result.userData;
 
             // Return the result of the authentication (true if successful, false otherwise)
-            return (isAuthenticated);
+            return isAuthenticated;
+
+        }
+
+        // Method to insert a new patient using the stored procedure gtspInsertPatient
+        // Returns a boolean indicating whether the insertion was successful or not
+        public async Task<bool> PatientCreateUser(string firstName, string lastName, string username, string password,
+            string email, string phone, string cpr, string address, int zipCode, string city, string imagePath)
+        {
+            try {
+                // Stored procedure name
+                var storedProcedure = "gtspInsertPatient";
+
+                byte[] imageBytes = (!string.IsNullOrEmpty(imagePath)) ? File.ReadAllBytes(imagePath) : null;
+
+                // Parameters for the stored procedure, using the properties of the Patient object
+                var parameters = new {
+                    CPR = cpr,
+                    FirstName = firstName,
+                    LastName = lastName,
+                    Username = username,
+                    Password = password,
+                    Email = email,
+                    Phone = phone,
+                    Address = address,
+                    ZipCode = zipCode,
+                    City = city,
+                    ProfilePicture = imageBytes
+                };
+
+                // Execute the stored procedure and retrieve the output parameter
+                var rowsAffected = await dbAccess.ExecuteNonQueryAsync(storedProcedure, parameters, CommandType.StoredProcedure);
+
+                return rowsAffected > 0;
+
+            } catch(Exception ex) {
+                Debug.WriteLine(ex.Message);
+                return false;
+            }
         }
 
         // Method to update patient profile information
@@ -89,8 +87,7 @@ namespace GTFys.ViewModels
 
                 byte[] imageBytes = (!string.IsNullOrEmpty(imagePath)) ? File.ReadAllBytes(imagePath) : null;
 
-                var parameters = new
-                {
+                var parameters = new {
                     CPR = cpr,
                     FirstName = firstName,
                     LastName = lastName,
@@ -102,38 +99,6 @@ namespace GTFys.ViewModels
                     ZipCode = zipCode,
                     City = city,
                     ProfilePicture = imageBytes
-
-            bool updateSuccessful = false;
-            try {
-                // SQL query to update patient profile in the "gtPATIENT" table
-                var query = "UPDATE gtPATIENT SET " +
-                    "CPR = @CPR, " +
-                    "FirstName = @FirstName, " +
-                    "LastName = @LastName, " +
-                    "Username = @Username, " +
-                    "Password = @Password, " +
-                    "Phone = @Phone, " +
-                    "Email = @Email, " +
-                    "Adress = @Address, " +
-                    "ZipCode = @ZipCode, " +
-                    "City = @City, " +
-                    "ProfilePicture = @ProfilePicture";
-
-
-                // Parameters for the SQL query, using the properties of the Patient object
-                var parameters = new {
-                    patient.CPR,
-                    patient.FirstName,
-                    patient.LastName,
-                    patient.Username,
-                    patient.Password,
-                    patient.Phone,
-                    patient.Email,
-                    patient.Address,
-                    patient.City,
-                    patient.ZipCode,
-                    // Assuming ProfilePicture property exists in the Patient class
-                    patient.ProfilePicture,
                 };
 
                 var rowsAffected = await dbAccess.ExecuteNonQueryAsync(query, parameters, CommandType.StoredProcedure);
