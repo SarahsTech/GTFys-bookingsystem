531badd63e4faff4b97f031c01fe382da853711f
Tobbekjaer <78278904+Tobbekjaer@users.noreply.github.com>
2023-12-04 14:21:00 +0100

    Added default constructor and minor changes in SetPrice()

--- GTFys/Models/Consultation.cs
+++ GTFys/Models/Consultation.cs
@@ -8,7 +8,7 @@ namespace GTFys.Models
 {
     // Enum to represent the treatment type 
     // The values represents the costs of the treatment type
-    public enum TreatmentTypes 
+    public enum TreatmentType 
     {
         FirstConsultation = 800,
         TrainingInstruction = 550
@@ -20,14 +20,19 @@ namespace GTFys.Models
         // Properties for the Patient, Physio, TreatmentType, and DateTime of the consultation
         public Patient Patient { get; set; }             // The patient involved in the consultation
         public Physio Physio { get; set; }               // The physiotherapist involved in the consultation
-        public TreatmentTypes TreatmentType { get; set; }// The type of treatment provided during the consultation
+        public TreatmentType TreatmentType { get; set; }// The type of treatment provided during the consultation
         public DateTime StartTime { get; set; }          // The date and starttime of the consultation
         public DateTime EndTime { get; set; }            // The date and endtime of the consultation
         public TimeSpan TimeSpan { get; set; }           // Timespan is calculated and set in the constructor, represents the length of a consultation
         public double Price { get; set; }                // Price is set in the constructur determined by the chosen treatment type
 
+        // Default constructor
+        public Consultation()
+        {
+            // Initialize any default values or perform additional setup logic if needed
+        }
         // Constructor to initialize Consultation properties
-        public Consultation(Patient patient, Physio physio, TreatmentTypes treatmenType, DateTime startTime)
+        public Consultation(Patient patient, Physio physio, TreatmentType treatmenType, DateTime startTime)
         {
             // Set the properties of the Consultation object based on the provided parameters
             Patient = patient;
@@ -53,11 +58,11 @@ namespace GTFys.Models
 
             // Update timespan based on TreatmentType
             switch (TreatmentType) {
-                case TreatmentTypes.FirstConsultation:
+                case TreatmentType.FirstConsultation:
                     // First consultation is 1 hour (60 minutes)
                     TimeSpan = TimeSpan.FromMinutes(60);
                     break;
-                case TreatmentTypes.TrainingInstruction:
+                case TreatmentType.TrainingInstruction:
                     // Training instruction is 45 minutes
                     TimeSpan = TimeSpan.FromMinutes(45);
                     break;
@@ -70,11 +75,11 @@ namespace GTFys.Models
         {
             // Set Price based on the chosen TreatmentType
             switch (TreatmentType) {
-                case TreatmentTypes.FirstConsultation:
-                    Price = 800;
+                case TreatmentType.FirstConsultation:
+                    Price = (double)TreatmentType.FirstConsultation; // Sets Price to 800
                     break;
-                case TreatmentTypes.TrainingInstruction:
-                    Price = 550;
+                case TreatmentType.TrainingInstruction:
+                    Price = (double)TreatmentType.TrainingInstruction; // Sets Price to 550
                     break;
                     // Add more cases if new treatment types are added in the future
             }
