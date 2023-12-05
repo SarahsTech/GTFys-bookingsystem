0c35cd8a2a0a9423c87dd6308714e4a6347e7b3d
Tobbekjaer <78278904+Tobbekjaer@users.noreply.github.com>
2023-12-05 09:47:45 +0100

    Changed foldername from Views to UI and adjusted all namespaces

--- GTFys/UI/PhysioFrontPageWindow.xaml.cs
+++ GTFys/UI/PhysioFrontPageWindow.xaml.cs
@@ -14,7 +14,7 @@ using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
 
-namespace GTFys.Views
+namespace GTFys.UI
 {
     /// <summary>
     /// Interaction logic for PhysioFrontPageWindow.xaml
--- GTFys/UI/PhysioLoginWindow.xaml.cs
+++ GTFys/UI/PhysioLoginWindow.xaml.cs
@@ -14,7 +14,7 @@ using System.Windows.Media;
 using System.Windows.Media.Imaging;
 using System.Windows.Shapes;
 
-namespace GTFys.Views
+namespace GTFys.UI
 {
     /// <summary>
     /// Interaction logic for PhysioLoginWindow.xaml
