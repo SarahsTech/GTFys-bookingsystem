ecb7b6512ca2c3fdfc2cdb154c54e58c91918c24
Tobbekjaer <78278904+Tobbekjaer@users.noreply.github.com>
2023-12-06 13:16:50 +0100

    Added LoadPatientGrid()

--- GTFys/UI/PatientsOverview.xaml.cs
+++ GTFys/UI/PatientsOverview.xaml.cs
@@ -1,5 +1,10 @@
-﻿using System;
+﻿using GTFys.Application;
+using Microsoft.Data.SqlClient;
+using System;
+using System.Collections;
 using System.Collections.Generic;
+using System.Data;
+using System.Diagnostics;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -23,6 +28,9 @@ namespace GTFys.UI
         public PatientsOverview()
         {
             InitializeComponent();
+
+            // Load patients from the database
+            LoadPatientGrid();
         }
 
         private void btnCreatePatient_Click(object sender, RoutedEventArgs e)
@@ -42,9 +50,56 @@ namespace GTFys.UI
 
         }
 
+        private void LoadPatientGrid()
+        {
+            try {
+                // Establish a new database connection
+                using (IDbConnection connection = new DatabaseConnection().Connect()) {
+
+                    string query = "gtspGetAllPatients";
+
+                    // Create a new SQL command using the provided query and connection
+                    using (SqlCommand command = new SqlCommand(query, (SqlConnection)connection)) {
+
+                        // Create a datatable and read from the database
+                        DataTable dt = new DataTable();
+                        SqlDataReader reader = command.ExecuteReader();
+                        // Load the content to the datatable
+                        dt.Load(reader);
+                        dgPatients.ItemsSource = dt.DefaultView;
+
+                        // Add the column names to the ComboBox
+                        foreach (DataRow dr in dt.Rows) {
+                            cbSearchPatient.Items.Add(dr.ToString());
+                        }
+
+                    }
+                }
+            }
+            catch (Exception ex) {
+                Debug.WriteLine(ex.Message);
+                MessageBox.Show("Der opstod en fejl ved indlæsning af patienter! \n" + ex.Message);
+            }
+        }
+
+        // Adds the values of the selected patient to textboxes
         private void dgPatients_SelectionChanged(object sender, SelectionChangedEventArgs e)
         {
-
+            // Converts the object type to a datagrid object
+            DataGrid dg = (DataGrid)sender;
+            // Casts the selected row in the datagrid to a DataRowView
+            DataRowView rowSelected = (DataRowView)dg.SelectedItem;
+            // Gets the values from the datagrid and adds it to their respective textboxes
+            if(rowSelected != null ) {
+                string name = rowSelected["Navn"].ToString();
+                string phone = rowSelected["Telefon"].ToString();
+                string email = rowSelected["Email"].ToString();
+                string cpr = rowSelected["CPR-nummer"].ToString();
+                tbName.Text = name;
+                tbPhone.Text = phone;
+                tbEmail.Text = email;
+                tbEmail.Text = cpr;
+            }
         }
 
         private void btnSearch_Click(object sender, RoutedEventArgs e)
