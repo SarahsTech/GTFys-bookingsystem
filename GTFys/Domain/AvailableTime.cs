e18a79364321acbf2d573506fe12f5ab8f8912a5
Sebastian <sebastian99private@icloud.com>
2023-12-12 09:40:34 +0100

    AvailableConsultationTime class created

---
+++ GTFys/Domain/AvailableTime.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GTFys.Domain
+{
+    class AvailableConsultationTime
+    {
+        public string FullName { get; set; }
+        public DateTime AvailableTime { get; set; }
+    }
+}
