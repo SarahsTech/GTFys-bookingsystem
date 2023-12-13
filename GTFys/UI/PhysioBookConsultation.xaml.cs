a24bced635748866cf7205ad879c990a6979633f
Tobbekjaer <78278904+Tobbekjaer@users.noreply.github.com>
2023-12-13 11:15:57 +0100

    AvailableTimes()

--- GTFys/UI/PhysioBookConsultation.xaml.cs
+++ GTFys/UI/PhysioBookConsultation.xaml.cs
@@ -120,12 +120,22 @@ namespace GTFys.UI
             return selectedDateTime;
         }
 
-
         private void SetPhysioIDs(out int physioID1, out int physioID2)
         {
-            physioID1 = cbPhysio1.IsChecked == true && cbPhysio2.IsChecked == true ? 1 : 2;
-            physioID2 = cbPhysio1.IsChecked == true && cbPhysio2.IsChecked == false ? 1 : 0;
-            physioID1 = cbPhysio1.IsChecked == false && cbPhysio2.IsChecked == true ? 1 : 0;
+            physioID1 = 0;
+            physioID2 = 0;
+
+            if (cbPhysio1.IsChecked == true && cbPhysio2.IsChecked == true) {
+                physioID1 = 1;
+                physioID2 = 2;
+                return; 
+            } else if (cbPhysio1.IsChecked == true && cbPhysio2.IsChecked == false) {
+                physioID1 = 1;
+                return; 
+            } else if(cbPhysio1.IsChecked == false && cbPhysio2.IsChecked == true) {
+                physioID1 = 2;
+                return; 
+            }
         }
         private (UITreatmentType, int) GetSelectedTreatmentTypeAndDuration()
         {
@@ -135,11 +145,11 @@ namespace GTFys.UI
             // Check which treatment type is selected and set duration
             if (rbFirstConsultation.IsChecked == true) {
                 treatmentType = UITreatmentType.FirstConsultation;
-                duration = 30; // Set the duration for FirstConsultation in minutes
+                duration = 60; // Set the duration for FirstConsultation in minutes
             }
             else if (rbTrainingInstruction.IsChecked == true) {
                 treatmentType = UITreatmentType.TrainingInstruction;
-                duration = 60; // Set the duration for TrainingInstruction in minutes
+                duration = 45; // Set the duration for TrainingInstruction in minutes
             }
 
             return (treatmentType, duration);
@@ -161,14 +171,18 @@ namespace GTFys.UI
 
         private void rbFirstConsultation_Checked(object sender, RoutedEventArgs e)
         {
-            // After updating the treatment type, call GetAvailableTimes
-            GetAvailableTimes();
+            if (calendarView.SelectedDate != null) {
+                // After updating the treatment type, call GetAvailableTimes
+                GetAvailableTimes();
+            }
         }
 
         private void rbTrainingInstruction_Checked(object sender, RoutedEventArgs e)
         {
-            // After updating the treatment type, call GetAvailableTimes
-            GetAvailableTimes();
+            if (calendarView.SelectedDate != null) {
+                // After updating the treatment type, call GetAvailableTimes
+                GetAvailableTimes();
+            }
         }
 
         private void btnSelectAll_Click(object sender, RoutedEventArgs e)
@@ -190,8 +204,10 @@ namespace GTFys.UI
 
         private void CheckboxSelectionChanged(object sender, RoutedEventArgs e)
         {
-            // After updating the checkboxes, call GetAvailableTimes
-            GetAvailableTimes();
+            if (calendarView.SelectedDate != null) {
+                // After updating the checkboxes, call GetAvailableTimes
+                GetAvailableTimes();
+            }
         }
 
         // Black out weekends for booking
@@ -237,53 +253,30 @@ namespace GTFys.UI
                 GetAvailableTimes();
             }
             else {
-                return; 
+                return;
             }
 
         }
 
         private async void GetAvailableTimes()
         {
-            int physioID1 = 0, physioID2 = 0, duration = 0;
+            // Use the new methods to get the necessary values
+            int physioID1, physioID2, duration;
             DateTime? date = calendarView.SelectedDate;
 
-            // Check which checkboxes are checked and set PhysioID's
-            if (!(cbPhysio1.IsChecked == true && cbPhysio2.IsChecked == true)) {
-                // Set PhysioID1 to 1 and PhysioID2 to 2
-                physioID1 = 1; 
-                physioID2 = 2;
-            }
-            else if (cbPhysio1.IsChecked == true && cbPhysio2.IsChecked == false) {
-                // Set PhysioID1 to 1
-                physioID1 = 1;
-            }
-            else if (cbPhysio2.IsChecked == true && cbPhysio1.IsChecked == false) {
-                // Set PhysioID2 to 2
-                physioID1 = 2;
-            }
-
-            // Check which treatment type is selected and set duration 
-            if (rbFirstConsultation.IsChecked == true) {
-                // Set duration to 60 
-                duration = 60; 
-            }
-            else if (rbTrainingInstruction.IsChecked == true) {
-                // Set duration to 45 
-                duration = 45;
-            }
+            SetPhysioIDs(out physioID1, out physioID2);
+            (UITreatmentType treatmentType, int tempDuration) = GetSelectedTreatmentTypeAndDuration();
+            duration = tempDuration;
 
             // If values are selected, display available times
-            if(physioID1 > 0 && date != null && duration > 0) {
-
+            if (physioID1 > 0 && date != DateTime.MinValue && duration > 0) {
                 try {
                     // Establish a new database connection
                     using (IDbConnection connection = new DatabaseConnection().Connect()) {
-
                         string query = "gtspGetAvailableConsultationTimes";
 
                         // Create a new SQL command using the provided query and connection
                         using (SqlCommand command = new SqlCommand(query, (SqlConnection)connection)) {
-
                             // Add parameters to the query
                             command.CommandType = CommandType.StoredProcedure;
                             command.Parameters.AddWithValue("@PhysioID1", physioID1);
@@ -297,7 +290,6 @@ namespace GTFys.UI
                             // Load the content to the datatable
                             dt.Load(reader);
                             dgAvailableTimes.ItemsSource = dt.DefaultView;
-
                         }
                     }
                 }
@@ -305,7 +297,7 @@ namespace GTFys.UI
                     Debug.WriteLine(ex.Message);
                     MessageBox.Show("Der opstod en fejl ved indlæsning af ledige tider! \n" + ex.Message);
                 }
-            }          
+            }
         }
 
         // TreatmentType to map the type of treatment
